84e5bc84edf42b532401ba7986663a0b7371be8d
m-ashourloo <moash76@gmail.com>
2022-02-17 14:37:26 +0330

    use environment base url in api service

--- src/app/comments/services/api.service.ts
+++ src/app/comments/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { CommentModel } from '../models/comments.model';
 import { UserModel } from '../models/user.model';
 
@@ -9,25 +10,27 @@ import { UserModel } from '../models/user.model';
 })
 export class ApiService {
 
+  baseUrl = environment.api;
+
   constructor(private httpClient: HttpClient) { }
 
   getComments(): Observable<CommentModel[]> {
-    return this.httpClient.get<CommentModel[]>('http://localhost:3000/comments');
+    return this.httpClient.get<CommentModel[]>(this.baseUrl + 'comments');
   }
 
   getCurrentUser(): Observable<UserModel> {
-    return this.httpClient.get<UserModel>('http://localhost:3000/currentUser');
+    return this.httpClient.get<UserModel>(this.baseUrl + 'currentUser');
   }
 
   editComment(comment: CommentModel): Observable<CommentModel> {
-    return this.httpClient.put<CommentModel>(`http://localhost:3000/comments/${comment.id}`, comment);
+    return this.httpClient.put<CommentModel>(this.baseUrl + `comments/${comment.id}`, comment);
   }
 
   addComment(comment: CommentModel) {
-    return this.httpClient.post(`http://localhost:3000/comments/`, comment);
+    return this.httpClient.post(this.baseUrl + `comments/`, comment);
   }
 
   deleteComment(comment: CommentModel){
-    return this.httpClient.delete(`http://localhost:3000/comments/${comment.id}`)
+    return this.httpClient.delete(this.baseUrl + `comments/${comment.id}`)
   }
 }
